8c13250fcd3a4f652f4ba347cbedacd0b9030071
exactchange <benny@exactchange.network>
2022-07-05 18:34:00 -0700

    move data path/uri to env var

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,23 @@
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
 const {
   NODE = '',
   API_KEY,
+  DATA_PATH,
   HOST,
   PORT
 } = process.env;
 
 const NODE_ENV = process.env.NODE_ENV || NODE.match(/home\//) ? 'development' : 'production';
+const DATA_URI = path.join(__dirname, DATA_PATH);
 
 module.exports = {
   API_KEY,
   NODE_ENV,
+  DATA_URI,
   HOST,
   PORT,
   URL: HOST
