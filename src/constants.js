405e530635ab4b1e7af5ee599cd7671a3394b291
exactchange <benny@exactchange.network>
2022-07-05 19:02:42 -0700

    lint

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ const {
 } = process.env;
 
 const NODE_ENV = process.env.NODE_ENV || NODE.match(/home\//) ? 'development' : 'production';
-const DB_URI = path.join(`${DATA_URI ? DATA_URI : __dirname}`, 'diamond', 'data');
+const DB_URI = path.join(`${DATA_URI || __dirname}`, 'diamond/data');
 
 module.exports = {
   NODE_ENV,
